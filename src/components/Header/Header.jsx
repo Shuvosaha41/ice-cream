bb4bf211ba83cb888e0661435c3b684f29349d94
Shuvosaha41 <ss09041@gmail.com>
2022-11-15 19:35:16 +0000

    update commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <div class="container react" id="header">
+          <div>
+            <img class="logo" src="images/logo.svg" alt="Logo" />
+          </div>
+          <div class="textRight">
+            <img src="images/react.svg" alt="React" class="reactLogo" /><strong
+              >React</strong>
+          </div>
+        </div>        
+      </header>
+  )
+}
